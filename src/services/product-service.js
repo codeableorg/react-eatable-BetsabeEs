c6753f412b7732a91d4246b2c3731df6a445b19b
BetsabeEs <roxana.developer.88@gmail.com>
2023-10-05 10:26:54 -0500

    Add: component create product

--- src/services/product-service.js
+++ src/services/product-service.js
@@ -3,3 +3,7 @@ import apiFetch from "./api-fetch";
 export function getProducts(){
     return apiFetch("/products");
 }
+
+export function createProducts(productData) {
+    return apiFetch("/products", { body: productData })
+}
